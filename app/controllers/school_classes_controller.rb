df9f2c63ccce49f5d86e7507d8584c8d513f0f86
GusVilla303 <gusvilla303@gmail.com>
2017-11-28 21:39:19 -0700

    Start to build StudentsController#create.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,8 +1,11 @@
 class SchoolClassesController < ApplicationController
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
+    binding.pry
+    #convert room_number to integer
   end
 
   def edit
