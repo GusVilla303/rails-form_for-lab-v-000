e3d54847f3d7e2c47069c994c7dbd06bcd5150f7
GusVilla303 <gusvilla303@gmail.com>
2017-11-29 21:50:54 -0700

    Add #new and #create actions to SchoolClassesController. Specs are passing.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,11 +1,23 @@
 class SchoolClassesController < ApplicationController
+
+  def show
+
+  end
   def new
     @school_class = SchoolClass.new
   end
 
   def create
-    binding.pry
-    #convert room_number to integer
+  #  binding.pry
+    @school_class = SchoolClass.new(title: params[:school_class][:title],
+                                    room_number: params[:school_class][:room_number]
+                                  )
+    #binding.pry
+    if @school_class.save
+      redirect_to school_class_path(@school_class)
+    else
+      render :new
+    end
   end
 
   def edit
